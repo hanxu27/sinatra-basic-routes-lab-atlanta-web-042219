ce98b368de653294578f3ac43abbcf8ad2c9c469
Han Xu <hanxu27@gmail.com>
2019-05-11 22:20:31 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is a random player"
+    end
+
+    get '/hometown' do
+        "My hometown is internetville"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is nye-cat"
+    end
 end
